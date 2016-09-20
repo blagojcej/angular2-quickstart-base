03ab2b20d2f0ed352517e44bdaa95ae442e28fbf
Blagojce Jovanoski <blagojcejovanoski@yahoo.com>
2016-09-20 09:44:49 +0200

    Added forms

---
+++ app/hero-form.component.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by blagojce on 20.09.2016.
+ */
+import { Component } from '@angular/core';
+import { Hero }    from './hero';
+@Component({
+  selector: 'hero-form',
+  templateUrl: 'app/hero-form.component.html'
+})
+export class HeroFormComponent {
+  powers = ['Really Smart', 'Super Flexible',
+    'Super Hot', 'Weather Changer'];
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  submitted = false;
+  onSubmit() { this.submitted = true; }
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+}
---
+++ app/hero.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by blagojce on 20.09.2016.
+ */
+export class Hero {
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public alterEgo?: string
+  ) {  }
+}
