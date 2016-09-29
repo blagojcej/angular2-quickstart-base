3b315474d77b6d33b5d77ae0ed9876f824ea65ac
Blagojce Jovanoski <blagojcejovanoski@yahoo.com>
2016-09-29 14:20:18 +0200

    Added new hero button and function.

--- app/hero-form.component.ts
+++ app/hero-form.component.ts
@@ -15,4 +15,8 @@ export class HeroFormComponent {
   onSubmit() { this.submitted = true; }
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
 }
