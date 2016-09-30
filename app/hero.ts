d6f8886c89afb3e26516c2147c974c87a84a7cf8
Blagojce Jovanoski <blagojcejovanoski@yahoo.com>
2016-09-30 07:34:53 +0200

    Readded hero.ts file.

---
+++ app/hero.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Blagojce on 30.9.2016.
+ */
+/**
+ * * Created by blagojce on 20.09.2016.
+ */
+export class Hero {
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public alterEgo?: string
+  ) {  }
+}
